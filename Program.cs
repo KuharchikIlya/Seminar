7a40b7b2b4645fe4b7b5a27865edbab478c49ea2
KuharchikIlya <svaps_srs@outlook.com>
2023-06-26 20:43:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12-> 2
+85 -> 8
+*/
+
+
+Random rand = new Random();
+
+int number = rand.Next (10,100);
+
+int firstNum = number/10;
+int secondNum = number%10;
+
+int max = firstNum;
+if (secondNum > firstNum) max = secondNum;
+
+//int answer = firstNum > secondNum ? firstNum : secondNum;
+
+Console.WriteLine($"Our random number is => {number}");
+Console.WriteLine($"First number is => {firstNum}");
+Console.WriteLine($"Second number is => {secondNum}");
+Console.WriteLine($"Bigger number is => {max}");
---
+++ Task_1/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+*/
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1%num2;
+if(result == 0)
+{
+    Console.WriteLine($"{num1} кратное {num2}");
+
+}
+else
+{
+    Console.WriteLine($"{num1} не кратное {num2}, остаток {result}");
+
+}
